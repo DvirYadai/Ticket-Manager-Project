150525df595c3ced250369c0dcf021bf0602c241
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-03-23 13:09:01 +0200

    requiring the ticket.js route to app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const ticketRoute = require("./routes/ticket");
 const app = express();
 
+app.use("/api/tickets", ticketRoute);
 app.use(express.static("client/build"));
 
 module.exports = app;
