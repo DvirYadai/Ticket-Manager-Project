956d2fe166622ba94fb6bcbba1a43f57489489cd
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-03-23 16:34:58 +0200

    adding Ticket Component

---
+++ client/src/Components/Ticket.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Ticket({ ticket }) {
+  console.log(ticket.labels);
+  return (
+    <div className="ticket">
+      <h1>{ticket.title}</h1>
+      <p>{ticket.content}</p>
+      <div className="ticket-info">
+        <p>{`By ${ticket.userEmail} | ${ticket.creationTime}`}</p>
+        {ticket.labels ? (
+          <ul>
+            {ticket.labels.map((label, i) => {
+              return (
+                <li className="label" key={`labelKey #${i}`}>
+                  {label}
+                </li>
+              );
+            })}
+          </ul>
+        ) : null}
+      </div>
+    </div>
+  );
+}
