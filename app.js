69bae3907abf10418e6fe8fe844be114d27a6a48
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-03-24 18:28:06 +0200

    adding morgan to add http logger to the server

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const ticketRoute = require("./routes/ticket");
+const morgan = require("morgan");
 const app = express();
 
 app.use(cors());
+morgan.token("type", (req) => {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(
+    ":method :url :status :response-time ms - :type :remote-addr [:date[clf]]"
+  )
+);
 app.use(express.json());
 app.use("/api/tickets", ticketRoute);
 app.use(express.static("client/build"));
